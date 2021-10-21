c78b194eb59b6b8486c620b2fa0c4e227485e893
aanyaanay <aanyaanay@aanyas-air.lan>
2021-10-21 09:58:42 -0700

    devops commit

--- src/user_data/user_login.java
+++ src/user_data/user_login.java
@@ -5,6 +5,7 @@ public class user_login {
 	public static void main(String[] args) {
 		System.out.println("Hi welcome");
 		System.out.println("Git commit");
+		System.out.println("Welcome to devops project");
 
 	}
 
